0591221e61cb79e082c089f1bdfc0e5a3365aa36
marco.cmm <marco.cmm@gmail.com>
2016-08-28 04:53:51 -0300

    fatura

--- app/models/fatura.rb
+++ app/models/fatura.rb
@@ -1,8 +1,8 @@
 class Fatura < ApplicationRecord
 	has_many :itens_fatura
 	belongs_to :beneficiario
-	scope :em_aberto, -> {where("pago = false AND data_vencimento >= ? ", Time.zone.now)}
+	scope :em_aberto, -> {where("pago = 0 AND data_vencimento >= ? ", Time.zone.now)}
 	scope :pagas, -> {where(:pago => true)}
-	scope :vencidas, -> {where("pago = false AND data_vencimento < ? ", Time.zone.now)}
+	scope :vencidas, -> {where("pago = 0 AND data_vencimento < ? ", Time.zone.now)}
 
 end
